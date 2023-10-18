8cec994279ca9c3aa1ca822f98f8eb6d32ffe02d
Xavier-Shaw <xshaw2002@gmail.com>
2023-10-18 16:48:30 -0700

    set up prudct pages (Tap4Shop & Tap4Ads)

--- src/containers/ProductAds.jsx
+++ src/containers/ProductAds.jsx
@@ -1,8 +1,44 @@
+import { CButton, CCol, CContainer, CHeader, CImage, CRow } from "@coreui/react";
+import "./ProductAds.css";
+
 export default function ProductAds() {
-    
+
     return (
-        <div>
-            
+        <div className="ads-div mx-auto">
+            <CContainer>
+                <CRow>
+                    <CCol sm={5} className="justify-div">
+                        <p className="fs-4 fw-bold">Tap4Ads</p>
+                        <p>Tap4Ads is a platform that ...</p>
+                        <CButton>立即尝试</CButton>
+                    </CCol>
+                    <CCol sm={7}>
+                        <CImage height={300} src="shopping-1.jpg" />
+                    </CCol>
+                </CRow>
+                <CHeader className="my-2" />
+                <CRow>
+                    <CCol sm={5} className="justify-div">
+                        <p className="fs-4 fw-bold">Function 1</p>
+                        <p>Tap4Ads can ...</p>
+                        <CImage height={200} src="shopping-2.png"/>
+                    </CCol>
+                    <CCol sm={7}>
+                        <CImage height={300} src="shopping-1.jpg" />
+                    </CCol>
+                </CRow>
+                <CHeader className="my-2" />
+                <CRow>
+                    <CCol sm={5} className="justify-div">
+                        <p className="fs-4 fw-bold">Function 2</p>
+                        <p>Tap4Ads can ...</p>
+                        <CImage height={200} src="shopping-2.png"/>
+                    </CCol>
+                    <CCol sm={7}>
+                        <CImage height={300} src="shopping-1.jpg" />
+                    </CCol>
+                </CRow>
+            </CContainer>
         </div>
     )
 }
--- src/containers/ProductShop.jsx
+++ src/containers/ProductShop.jsx
@@ -1,8 +1,45 @@
+import { CButton, CCol, CContainer, CHeader, CImage, CRow } from "@coreui/react";
+import "./ProductShop.css";
+import CIcon from "@coreui/icons-react";
+import { cibAndroidAlt } from "@coreui/icons";
+
 export default function ProductShop() {
-    
+
     return (
-        <div>
-            
+        <div className="shop-div mx-auto">
+            <CContainer>
+                <CRow>
+                    <CCol sm={5} className="justify-div">
+                        <p className="fs-4 fw-bold">Tap4Shop</p>
+                        <p>Tap4Shop is an application that ...</p>
+                        <CButton>
+                            <CIcon icon={cibAndroidAlt} />
+                            下载安卓应用
+                        </CButton>
+                    </CCol>
+                    <CCol sm={7}>
+                        <CImage height={300} src="shopping-1.jpg" />
+                    </CCol>
+                </CRow>
+                <CHeader className="my-2" />
+                <CRow>
+                    <CCol sm={4} className="justify-div">
+                        <p className="fs-4 fw-bold">Function 1</p>
+                        <p>Tap4Shop can ...</p>
+                        <CImage height={200} src="shopping-2.png" />
+                    </CCol>
+                    <CCol sm={4} className="justify-div">
+                        <p className="fs-4 fw-bold">Function 2</p>
+                        <p>Tap4Shop can ...</p>
+                        <CImage height={200} src="shopping-2.png" />
+                    </CCol>
+                    <CCol sm={4} className="justify-div">
+                        <p className="fs-4 fw-bold">Function 3</p>
+                        <p>Tap4Shop can ...</p>
+                        <CImage height={200} src="shopping-2.png" />
+                    </CCol>
+                </CRow>
+            </CContainer>
         </div>
     )
 }
