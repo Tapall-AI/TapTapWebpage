3899126dcc0055fa2aa6d4375ebbb6194a963c8e
Xavier-Shaw <xshaw2002@gmail.com>
2023-10-20 16:37:28 -0700

    set up about page

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,8 +1,16 @@
+import { CHeader } from "@coreui/react";
+import "./About.css";
+
 export default function About() {
     
     return (
-        <div>
-            
+        <div className="about-div mx-auto">
+            <p className="fs-2 fw-bold">关于我们</p>
+            <p className="fs-4">我们是VIP Lab, 我们正在研发一款...</p>
+            <CHeader className="my-2"/>
+            <p className="fs-2 fw-bold">我们的愿景...</p>
+            <CHeader className="my-2"/>
+            <p className="fs-2 fw-bold">我们的价值观...</p>
         </div>
     )
 }
